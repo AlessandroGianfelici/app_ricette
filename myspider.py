c8b5a299693bd80e518968ca0a04b8fef4895f45
Gianfelici <gianfea@CORP.EDISON.IT>
2022-01-21 21:03:38 +0100

    resi parametrici gli url

--- myspider.py
+++ myspider.py
@@ -2,21 +2,28 @@
 import json
 import os
 
-import scrapy
+import numpy as np
+import pandas as pd
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from source.functions import select_or_create
 from source.parser import parse_recipe
-import os
+from urllib.parse import urlparse
 
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
 OUTPUTPATH = select_or_create(os.path.join(APP_PATH, 'data', 'json'))
-#https://github.com/hhursev/recipe-scrapers
+URLS = list(np.squeeze(
+                 pd.read_csv(
+                     os.path.join(APP_PATH, "data", "supported_websites.csv"), 
+                     header=None).values))
+
+DOMAINS = list(map(lambda x : urlparse(x).netloc, URLS))
+
 class MySpider(CrawlSpider):
     name = 'gspider'
-    allowed_domains = ['themodernproper.com']
-    start_urls = ['https://themodernproper.com']
+    allowed_domains = DOMAINS
+    start_urls = URLS
     rules = (# Extract and follow all links!
         Rule(LinkExtractor(), callback='parse_item', follow=True), )
                
