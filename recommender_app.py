46bc7cf3c440ac129b6fef5889620ad8a99ee369
Gianfelici <gianfea@CORP.EDISON.IT>
2022-01-23 11:31:24 +0100

    correzini alla app di raccomandazione

--- recommender_app.py
+++ recommender_app.py
@@ -1,18 +1,28 @@
 from flask import Flask, jsonify, request
+from flask import abort, redirect, url_for
+
 from source.recommender import recommend_recipes
 import re
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
-app.config["DEBUG"] = True
+app.config["DEBUG"] = False
 
 def preprocess_query(query : str):
+    """
+    Utility function to remove invalid
+    characters from the query.
+    """
     return re.sub(r'[^a-zA-Z]', ' ', query).strip().replace("_", " ")
+
+@app.route('/')
+def index():
+    return redirect(url_for('suggest_recipe'))
 
 @app.route('/v1/suggest_recipe', methods=['GET'])
 def suggest_recipe():
     if "ingredients" not in request.args:
-        return 'url parameter is required', 400
+        return 'ingredients parameter is required', 400
     else:
         if "limit" not in request.args:
             max_results = 5
